e3b9cbd4018d4787b8d16b391d58e9945d5ca350
BetweenTwoTests <BetweenTwoTests>
2023-06-11 17:28:19 -0700

    Using event to capture loop result

--- __tests__/nearley-newick.test.js
+++ __tests__/nearley-newick.test.js
@@ -6,6 +6,6 @@ const fs = require('fs');
 const assert = require('assert').strict;
 
 const ex2 = fs.readFileSync('__tests__/examples/ex-2-all-nodes-named.tree', 'utf8')
-nn.newickToJSON(ex2);
+console.log(JSON.stringify(nn.newickToJSON(ex2)));
 
 console.info('nearley-newick tests passed');
